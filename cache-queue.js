58a6363fa80acba95c3aba08a0d9c9eb0a277385
johnytiago <joaosao10@gmail.com>
2018-08-08 18:11:48 +0100

    Runs prettier, adds process method

--- cache-queue.js
+++ cache-queue.js
@@ -1,33 +1,37 @@
-'use strict'
+'use strict';
 
 module.exports = class cacheQueue {
-  constructor (self) {
-    this._entries = []
+  constructor(self) {
+    this._entries = [];
   }
 
-  addEntry (cid, peer) {
-    this._entries = this._entries.push({ peer, cid })
-    this._get(peer, cid)
+  addEntry(cid, cb) {
+    this._entries.push(cid);
+    this._get(cid, cb);
   }
 
-  removeEntry (cid) {
-    const index = this._entries.findIndex( entry  => cid.equals(entry.cid) )
+  removeEntry(cid) {
+    const index = this._entries.findIndex(entry => cid.equals(entry.cid));
 
-    if (index === -1)
-      return
+    if (index === -1) return;
 
-    this._entries.splice(index, 1)
+    this._entries.splice(index, 1);
   }
 
-  _get(peer, cid) {
+  _get(cid, cb) {
     self.bitswap.get(cid, (err, block) => {
-      if (err)
-        // try again ?
+      // try again ?
+      if (err) return cb(err);
+
       // dht set ourselves as providers of the block
-      self.dht.provide(cid, (err) => {
-        if (err)
-          // could not set provide, try again?
+      self.dht.provide(cid, err => {
+        // could not set provide, try again?
+        if (err) return cb(err);
+
         // successful
-    }) 
+        removeEntry(cid);
+        return cb(null);
+      });
+    });
   }
-}
+};
--- index.js
+++ index.js
@@ -1,14 +1,46 @@
-const CacheQueue = require('./cache-queue')
+const waterfall = require('async/waterfall');
+
+const CacheQueue = require('./cache-queue');
 // import popularity calculator
 
 class Startrail {
   constructor(libp2p, blockstore, dht) {
-    this.libp2p = libp2p
-    this.blockstore = blockstore
-    this.dht = dht
+    this.libp2p = libp2p;
+    this.blockstore = blockstore;
+    this.dht = dht;
 
-    this cacheQueue = new CacheQueue(this)
+    this.cacheQueue = new CacheQueue(this);
+  }
+
+  process({ cid, peer }, cb) {
+    waterfall(
+      [
+        // update popularity
+        // keep track of peers that asked for it
+        cb => {
+          return cb(null, true); // mock calculation
+        },
+        // popular ? Check blockstore for cid
+        (popular, cb) => {
+          if (!popular) return cb('E_NOT_POPULAR'); // break waterfall, not popular
+
+          // Evaluate level of popularity
+          return this.blockstore.has(cid, cb);
+        },
+        // not in store ? Fetch from the network
+        (has, cb) => {
+          if (has) return cb(null);
+
+          return this.cacheQueue.addEntry(cid);
+        }
+      ],
+      (err, result) => {
+        if (err && err !== 'E_NOT_POPULAR') cb(err);
+
+        cb(null);
+      }
+    );
   }
 }
 
-module.exports = Startrail
+module.exports = Startrail;
